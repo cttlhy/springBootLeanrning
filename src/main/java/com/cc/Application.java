e8640eb85dca705e20625a1ec4e61bfc0c79f86c
https://github.com/cttlhy/springBootLeanrning.git <2539131080@qq.com>
2017-03-29 17:20:29 +0800

    springboot上传文件

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -1,8 +1,11 @@
 package com.cc;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -34,4 +37,21 @@ public class Application {
 		HttpMessageConverter<?> converter = fastConverter;
 		return new HttpMessageConverters(converter);
 	}
+	
+	/**
+	 * 文件上传大小限制
+	 * @return
+	 */
+	@Bean 
+	public MultipartConfigElement multipartConfigElement() { 
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
+        factory.setMaxFileSize("128KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("256KB"); 
+        //Sets the directory location wherefiles will be stored.
+        factory.setLocation("d:/imgFile");
+        return factory.createMultipartConfig(); 
+
+    } 
 }
