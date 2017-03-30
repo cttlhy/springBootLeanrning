1eaf5569c5a827862c684b9835b952dc8e26ff06
chenchao <chenchao@chenchao-PC>
2017-03-30 18:19:11 +0800

    commit

--- src/main/java/com/cc/sys/core/schedul/SysHelloSchedul.java
+++ src/main/java/com/cc/sys/core/schedul/SysHelloSchedul.java
@@ -1,7 +1,11 @@
 package com.cc.sys.core.schedul;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import com.cc.sys.core.listener.TestServletContextListener;
 
 /**
  * 定时调度任务
@@ -10,7 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
  */
 @Configuration
 public class SysHelloSchedul {
-
+	private static final Logger logger = LoggerFactory.getLogger(TestServletContextListener.class);
 	/**
 	 * 					秒      分      时       周   月   年
 	 * @Scheduled(cron="0 0/2 8-20 * * ?") 每天早八点到晚八点，间隔2分钟执行任务
@@ -21,7 +25,7 @@ public class SysHelloSchedul {
 	@Scheduled(cron = "0/5 * * * * ?") // 每5秒执行一次
 	public void sayHelloForU() {
 		Thread current = Thread.currentThread();
-		System.out.println("Hello, My Friend! 线程ID["+ current.getId()+"]向您问好！");
+		logger.info("线程调度类!【com.cc.sys.core.schedul.SysHelloSchedul】 线程ID["+ current.getId()+"]向您问好！");
 	}
 
 }
