10784fa903157084441f91714fba5fa880a9794b
chenchao <chenchao@chenchao-PC>
2017-03-31 11:22:17 +0800

    添加实现类方法

--- src/main/java/com/cc/sys/core/service/UserService.java
+++ src/main/java/com/cc/sys/core/service/UserService.java
@@ -1,15 +1,19 @@
 package com.cc.sys.core.service;
 
-import java.sql.SQLException;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.cc.sys.core.dao.UserDao;
+import com.cc.sys.core.dto.SysUser;
 
 @Service
 public class UserService {
+	@Resource
+	private UserDao userDao;
 	
+	public SysUser selectUserById(String id){
+		return userDao.selectUserById(id);
+	}
 
 }
