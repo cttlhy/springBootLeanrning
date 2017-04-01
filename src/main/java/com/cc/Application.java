ff4584f0fa9f33982e689487b1e8b786fc529c39
chenchao <chenchao@chenchao-PC>
2017-04-01 14:17:53 +0800

    修改了主类对打成war包的支持

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -5,8 +5,10 @@ import javax.servlet.MultipartConfigElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
@@ -18,11 +20,23 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 @SpringBootApplication
 // @EnableScheduling//启动定时任务
 // @Import(User.class)//用 import注解可以将非扫描包下的bean被spring管理
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	/**
+	 * 可实现打成war包在tomcat中部署
+	 * 注意:	1.该主类需要继承SpringBootServletInitializer类并且重写该方法
+	 * 		2.在pom.xml中将org.apache.tomcat.embed 的 scope 修改为 provided
+	 * 		3.在pom.xml中的bulid标签中，添加最终打包名称用来tomcat部署时的上下文名称
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // TODO Auto-generated method stub
+        return builder.sources(Application.class);
+    }
 
 	/**
 	 * 使用fastjson作为转换器
