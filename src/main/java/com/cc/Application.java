4b498ac91394db63eaa89c23c905357fae3a665c
chenchao <chenchao@chenchao-PC>
2017-04-01 14:21:33 +0800

    format

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -20,26 +20,26 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 @SpringBootApplication
 // @EnableScheduling//启动定时任务
 // @Import(User.class)//用 import注解可以将非扫描包下的bean被spring管理
-public class Application extends SpringBootServletInitializer{
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 	/**
-	 * 可实现打成war包在tomcat中部署
-	 * 注意:	1.该主类需要继承SpringBootServletInitializer类并且重写该方法
-	 * 		2.在pom.xml中将org.apache.tomcat.embed 的 scope 修改为 provided
-	 * 		3.在pom.xml中的bulid标签中，添加最终打包名称用来tomcat部署时的上下文名称
+	 * 可实现打成war包在tomcat中部署 注意: 1.该主类需要继承SpringBootServletInitializer类并且重写该方法
+	 * 2.在pom.xml中将org.apache.tomcat.embed 的 scope 修改为 provided
+	 * 3.在pom.xml中的bulid标签中，添加最终打包名称用来tomcat部署时的上下文名称
 	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // TODO Auto-generated method stub
-        return builder.sources(Application.class);
-    }
+		// TODO Auto-generated method stub
+		return builder.sources(Application.class);
+	}
 
 	/**
 	 * 使用fastjson作为转换器
+	 * 
 	 * @return
 	 */
 	@Bean
@@ -51,21 +51,22 @@ public class Application extends SpringBootServletInitializer{
 		HttpMessageConverter<?> converter = fastConverter;
 		return new HttpMessageConverters(converter);
 	}
-	
+
 	/**
 	 * 文件上传大小限制
+	 * 
 	 * @return
 	 */
-	@Bean 
-	public MultipartConfigElement multipartConfigElement() { 
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        //// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
-        factory.setMaxFileSize("128KB"); //KB,MB
-        /// 设置总上传数据总大小
-        factory.setMaxRequestSize("256KB"); 
-        //Sets the directory location wherefiles will be stored.
-       // factory.setLocation("d:/imgFile");
-        return factory.createMultipartConfig(); 
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
+		factory.setMaxFileSize("128KB"); // KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("256KB");
+		// Sets the directory location wherefiles will be stored.
+		// factory.setLocation("d:/imgFile");
+		return factory.createMultipartConfig();
 
-    } 
+	}
 }
