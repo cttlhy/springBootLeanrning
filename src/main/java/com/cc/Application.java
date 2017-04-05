26b44cc5b30f3390d25bf5c5b72f700905885130
chenchao <chenchao@chenchao-PC>
2017-04-05 16:53:43 +0800

    修改本类，主要添加了如何获取 事务管理器 ，主要用于测试打印观察 spring容器对各个事务管理器的生成

--- src/main/java/com/cc/Application.java
+++ src/main/java/com/cc/Application.java
@@ -1,7 +1,11 @@
 package com.cc;
 
+import javax.annotation.Resource;
 import javax.servlet.MultipartConfigElement;
+import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
@@ -9,8 +13,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
@@ -18,12 +26,44 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 @ServletComponentScan
 @SpringBootApplication
+@EnableTransactionManagement
 // @EnableScheduling//启动定时任务
-// @Import(User.class)//用 import注解可以将非扫描包下的bean被spring管理
-public class Application extends SpringBootServletInitializer {
+// @Import(Dept.class)//用 import注解可以将非扫描包下的bean被spring管理
+public class Application extends SpringBootServletInitializer  {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	@Resource(name="tx11")
+	private PlatformTransactionManager tx11;
+	
+	@Bean(name = "tx11")
+	public PlatformTransactionManager txManager(DataSource dataSource) {
+		logger.info("===============事务管理1加载完毕-->tx1:" + dataSource.getClass().getName());
+		return new DataSourceTransactionManager(dataSource);
+	}
+	/*
+	@Resource(name = "tx22")
+	private PlatformTransactionManager tx22;
+
+	
+
+	// 创建事务管理器2
+	@Bean(name = "tx22")
+	public PlatformTransactionManager txManager2(EntityManagerFactory factory) {
+		logger.info("===============事务管理2加载完毕-->tx2:" + factory.getClass().getName());
+		return new JpaTransactionManager(factory); 
+	}
+
+	// 实现接口 TransactionManagementConfigurer 方法，其返回值代表在拥有多个事务管理器的情况下默认使用的事务管理器
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		logger.info("tx11-->"+tx11.getClass().getName());
+		logger.info("tx22-->"+tx22.getClass().getName());
+		return tx11;
+	}*/
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext  ctx = SpringApplication.run(Application.class, args);
 	}
 
 	/**
@@ -69,4 +109,5 @@ public class Application extends SpringBootServletInitializer {
 		return factory.createMultipartConfig();
 
 	}
+
 }
